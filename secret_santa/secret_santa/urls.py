bd0854381bfc47721437b727997a3e494e949a1b
Kevin Xu <kxu@veraudio.com>
2022-12-03 16:06:56 -0500

    server static files

--- secret_santa/secret_santa/urls.py
+++ secret_santa/secret_santa/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,5 @@ urlpatterns += [
 urlpatterns += [
     path('', RedirectView.as_view(url='ssweb', permanent=True))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
