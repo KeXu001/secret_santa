07048cc0cb41e02edc6b1e84d4548ab6377a0368
Kevin Xu <>
2023-11-14 01:22:44 +0000

    add budget field

--- secret_santa/ssweb/models/game.py
+++ secret_santa/ssweb/models/game.py
@@ -6,6 +6,7 @@ from django.db import models
 
 class Game(models.Model):
     name = models.CharField(max_length=100)
+    budget = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
         return self.name
